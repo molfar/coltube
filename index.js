88572437ae626352261ade4d9dd56ccf038068d8
Molfar <terryprogetto@gmail.com>
2022-12-27 17:37:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const ColorThief = require("colorthief");
+const { program } = require("commander");
+
+const { rgbToHex } = require("./lib/utils");
+
+program
+  .name("coltube")
+  .description("CLI to extract color palette from image")
+  .version(process.env.npm_package_version)
+  .argument("<url>", "Public accessible url to image sourcet")
+  .option("-l, --limit <number>", "Number of colors to extract", parseInt, 5)
+  .action((imageUrl, options) => {
+    ColorThief.getPalette(imageUrl, options.limit)
+      .then((palette) => {
+        console.log(palette.map(rgbToHex));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+program.parse();
